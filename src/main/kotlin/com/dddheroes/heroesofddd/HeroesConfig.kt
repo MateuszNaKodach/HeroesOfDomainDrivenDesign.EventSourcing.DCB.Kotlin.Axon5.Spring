bae65e5721c771ed704ef48db32d7e7c05f54cd8
Mateusz Nowak <mateusz@nakodach.pl>
2025-07-01 12:04:53 +0200

    tag resolver for metadata

--- src/main/kotlin/com/dddheroes/heroesofddd/HeroesConfig.kt
+++ src/main/kotlin/com/dddheroes/heroesofddd/HeroesConfig.kt
@@ -1,19 +1,28 @@
 package com.dddheroes.heroesofddd
 
+import com.dddheroes.heroesofddd.shared.application.GameMetaData
 import org.axonframework.configuration.ConfigurationEnhancer
+import org.axonframework.eventsourcing.eventstore.MetaDataBasedTagResolver
+import org.axonframework.eventsourcing.eventstore.MultiTagResolver
 import org.axonframework.eventsourcing.eventstore.TagResolver
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class HeroesConfig {
+internal class HeroesConfig {
 
     @Bean
-    fun metaDataTagResolverEnhancer(): ConfigurationEnhancer = ConfigurationEnhancer { configuration ->
+    fun tagResolverEnhancer(): ConfigurationEnhancer = ConfigurationEnhancer { configuration ->
         configuration.registerDecorator(
             TagResolver::class.java,
             Integer.MAX_VALUE
-        ) { c, n, d -> d }
+        ) { c, n, d ->
+            MultiTagResolver(
+                MetaDataBasedTagResolver(GameMetaData.GAME_ID_KEY),
+                MetaDataBasedTagResolver(GameMetaData.PLAYER_ID_KEY),
+                d
+            )
+        }
     }
 
 
