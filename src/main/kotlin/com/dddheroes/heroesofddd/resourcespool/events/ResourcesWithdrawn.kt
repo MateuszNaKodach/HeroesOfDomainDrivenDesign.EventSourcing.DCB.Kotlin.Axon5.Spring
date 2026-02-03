0a5e130c4a56bf69a7ef71bfe07b4684a034b82d
Mateusz Nowak <mateusz@nakodach.pl>
2026-02-03 11:41:06 +0100

    ♻️ refactor: add a note to consider replacing `String` with `ResourceType` in resources events

--- src/main/kotlin/com/dddheroes/heroesofddd/resourcespool/events/ResourcesWithdrawn.kt
+++ src/main/kotlin/com/dddheroes/heroesofddd/resourcespool/events/ResourcesWithdrawn.kt
@@ -4,5 +4,5 @@ import com.dddheroes.heroesofddd.shared.domain.valueobjects.ResourcesPoolId
 
 data class ResourcesWithdrawn(
     override val resourcesPoolId: ResourcesPoolId,
-    val resources: Map<String, Int>
+    val resources: Map<String, Int> // todo: change String to ResourceType?
 ) : ResourcesPoolEvent
