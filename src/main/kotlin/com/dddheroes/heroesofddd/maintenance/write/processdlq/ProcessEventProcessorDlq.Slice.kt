535e6f33e9db56b0862aaa0df0ba50a7fe943a87
Mateusz Nowak <mateusz@nakodach.pl>
2026-02-05 02:04:07 +0100

    ✨ feat: add DLQ processing endpoint for event processors
    - Introduced a REST API to enable DLQ processing for specific event processors by name.
    - Leveraged `SequencedDeadLetterProcessor` for processing dead letters asynchronously.
    - Included error handling for invalid processor names.

---
+++ src/main/kotlin/com/dddheroes/heroesofddd/maintenance/write/processdlq/ProcessEventProcessorDlq.Slice.kt
@@ -0,0 +1,28 @@
+package com.dddheroes.heroesofddd.maintenance.write.processdlq
+
+import org.axonframework.common.configuration.Configuration
+import org.axonframework.messaging.deadletter.SequencedDeadLetterProcessor
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.CompletableFuture
+import kotlin.jvm.optionals.getOrNull
+
+
+@RestController
+private class ProcessEventProcessorDlqRestApi(private val configuration: Configuration) {
+
+    @PutMapping("/maintenance/processors/{processorName}/dlq-processing")
+    fun putDwellings(
+        @PathVariable processorName: String,
+    ): CompletableFuture<ResponseEntity<Any>> {
+        val processorModule =
+            configuration.getModuleConfiguration(processorName).getOrNull()
+                ?: return CompletableFuture.completedFuture(ResponseEntity.notFound().build())
+
+        val dlqProcessors = processorModule.getComponents(SequencedDeadLetterProcessor::class.java).values
+        return CompletableFuture.allOf(*dlqProcessors.map { it.processAny() }.toTypedArray())
+            .thenApply { ResponseEntity.ok().build() }
+    }
+}
