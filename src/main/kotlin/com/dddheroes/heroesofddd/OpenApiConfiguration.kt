a15a623b8865b5c96beb3692a5207c86370e4171
Mateusz Nowak <mateusz@nakodach.pl>
2026-03-07 17:50:19 +0100

    Revert "📝 docs: add `x-player-id` header to OpenAPI specification"
    This reverts commit 410e966ff83943c52b7b137169b17451461d793e.

--- src/main/kotlin/com/dddheroes/heroesofddd/OpenApiConfiguration.kt
+++ src/main/kotlin/com/dddheroes/heroesofddd/OpenApiConfiguration.kt
@@ -1,11 +1,7 @@
 package com.dddheroes.heroesofddd
 
-import com.dddheroes.heroesofddd.shared.restapi.Headers
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
-import io.swagger.v3.oas.models.parameters.HeaderParameter
-import io.swagger.v3.oas.models.media.StringSchema
-import org.springdoc.core.customizers.OperationCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -20,17 +16,5 @@ class OpenApiConfiguration {
                 .description("REST API for Heroes of Might & Magic III - DDD, Event Sourcing, Event Modeling")
                 .version("1.0.0")
         )
-
-    @Bean
-    fun playerIdHeaderCustomizer(): OperationCustomizer = OperationCustomizer { operation, _ ->
-        operation.addParametersItem(
-            HeaderParameter()
-                .name(Headers.PLAYER_ID)
-                .description("Unique identifier of the player issuing the request")
-                .required(true)
-                .schema(StringSchema())
-        )
-        operation
-    }
 
 }
