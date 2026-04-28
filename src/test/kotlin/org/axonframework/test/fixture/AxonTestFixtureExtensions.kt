348a62b0678c05b8eeaccaf055bea99971a337b4
Mateusz Nowak <mateusz@nakodach.pl>
2026-04-28 13:15:28 +0200

    ♻️ refactor: remove unused `springTestFixture` function in Axon test extensions

--- src/test/kotlin/org/axonframework/test/fixture/AxonTestFixtureExtensions.kt
+++ src/test/kotlin/org/axonframework/test/fixture/AxonTestFixtureExtensions.kt
@@ -1,12 +1,6 @@
 package org.axonframework.test.fixture
 
 import org.axonframework.common.configuration.ApplicationConfigurer
-import org.axonframework.common.configuration.AxonConfiguration
-
-fun springTestFixture(
-    configuration: AxonConfiguration,
-    customization: AxonTestFixture.Customization = AxonTestFixture.Customization()
-) = AxonTestFixture(configuration, customization)
 
 fun axonTestFixture(configurer: ApplicationConfigurer): AxonTestFixture =
     AxonTestFixture.with(configurer) { customization ->
